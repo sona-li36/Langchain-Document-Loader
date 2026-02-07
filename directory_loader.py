0ab5e9507172d73f942ae6e2bdb33d6fb808e34a
Sonali kiran <sonalikiran0630@gmail.com>
2026-02-07 16:21:00 +0530

    loader and lazy loader

--- directory_loader.py
+++ directory_loader.py
@@ -5,8 +5,11 @@ loader =DirectoryLoader(
     glob='*.pdf',
     loader_cls= PyPDFLoader
 )
-docs = loader.load()
+#docs = loader.load()
+docs = loader.lazy_load()
+# print(len(docs))
+#print(docs[0].page_content)
+#print(docs[0].metadata)
 
-print(len(docs))
-print(docs[0].page_content)
-print(docs[0].metadata)
+for document in docs:
+    print(document.metadata)
